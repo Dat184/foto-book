b8afc71da5afa817739ae436b7fa6eb3d5f76b81
DatNT <thanhnguyendat184@gmail.com>
2026-01-19 08:56:09 +0700

    Add column role to user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_01_19_015213) do
+ActiveRecord::Schema[8.1].define(version: 2026_01_19_015436) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -36,6 +36,7 @@ ActiveRecord::Schema[8.1].define(version: 2026_01_19_015213) do
     t.string "firtsName"
     t.string "lastName"
     t.string "password"
+    t.integer "role", default: 0, null: false
     t.datetime "updated_at", null: false
   end
 end
