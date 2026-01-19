43947365c2aa54d8e800d871f68b6797fde2fb8a
DatNT <thanhnguyendat184@gmail.com>
2026-01-19 08:50:06 +0700

    fix error enum in albums model

--- app/models/album.rb
+++ app/models/album.rb
@@ -1,3 +1,5 @@
 class Album < ApplicationRecord
-  enum sharing: { public: 0, private: 1 }
+  # Cú pháp: enum :tên_cột, { hash_giá_trị }, option
+  # Lưu ý: dùng "prefix: true" thay vì "_prefix: true" (dù cả 2 đều chạy, nhưng prefix chuẩn hơn ở cú pháp mới)
+  enum :sharing, { public: 0, private: 1 }, prefix: true
 end
