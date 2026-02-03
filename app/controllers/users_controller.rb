95c0b4449fdb584b40ea184975f1527cc042c895
DatNT <thanhnguyendat184@gmail.com>
2026-02-03 17:24:30 +0700

    Config devise to sign-in and sign-up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -67,6 +67,6 @@ class UsersController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def user_params
-      params.expect(user: [ :firtsName, :lastName, :email, :password ])
+      params.expect(user: [ :firstName, :lastName, :email, :password ])
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_02_02_071700) do
+ActiveRecord::Schema[8.1].define(version: 2026_02_03_102308) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -59,15 +59,16 @@ ActiveRecord::Schema[8.1].define(version: 2026_02_02_071700) do
     t.string "confirmation_token"
     t.datetime "confirmed_at"
     t.datetime "created_at", null: false
-    t.datetime "current_sign_in_at"
     t.string "email"
     t.string "encrypted_password", default: "", null: false
-    t.string "firtsName"
+    t.string "firstName"
     t.string "lastName"
+    t.datetime "last_sign_in_at"
     t.datetime "remember_created_at"
     t.datetime "reset_password_sent_at"
     t.string "reset_password_token"
     t.integer "role", default: 0, null: false
+    t.string "unconfirmed_email"
     t.datetime "updated_at", null: false
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
