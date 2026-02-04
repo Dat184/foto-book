08ce1059546536e278263faa8fc67d195809ef91
DatNT <thanhnguyendat184@gmail.com>
2026-02-04 15:44:47 +0700

    Configure for devise to send mail with activejob

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable, :confirmable
   enum :role, { user: 0, admin: 1 }, prefix: true
+
+  def send_devise_notification(notification, *args)
+    devise_mailer.send(notification, self, *args).deliver_later
+  end
+
   # Associations for follows
   has_many :active_follows, class_name: "Follow", foreign_key: "follower_id", dependent: :destroy
   has_many :following, through: :active_follows, source: :followed
