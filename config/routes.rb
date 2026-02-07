29f153b9a5147db7a2f30a37664a2ac0f5456e1b
Dat184 <thanhnguyendat184@gmail.com>
2026-02-07 21:24:35 +0700

    Feature go to see public profile of the other user. Action follow or unfollow user

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
 
   get "profile" => "users#profile", as: :profile
   get "profile/edit" => "users#edit_profile", as: :edit_profile
+  get "users/:id/profile" => "users#public_profile", as: :public_profile
+  post "users/:id/follow" => "users#follow", as: :follow_user
+  delete "users/:id/unfollow" => "users#unfollow", as: :unfollow_user
 
   resources :albums
   resources :photos
